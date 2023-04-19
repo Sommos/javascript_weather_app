a999b8d8de6fd60d9869fdc626e12b9734e5ae0a
Sommos <Sommosgaming@gmail.com>
2023-04-19 15:51:10 +0100

    added comments to index.js

--- index.js
+++ index.js
@@ -4,18 +4,23 @@ const weatherBox = document.querySelector('.weather-box');
 const weatherDetails = document.querySelector('.weather-details');
 const error404 = document.querySelector('.not-found');
 
+// add event listener to search button on click action
 search.addEventListener('click', () => {
+    // add your api key here
     const apiKey = 'openweathermap_api_key';
     const city = document.querySelector('.search-box input').value;
 
+    // if city search is empty, return nothing
     if(city === '') {
         return;
     }
 
+    // fetcg json from openweathermap api
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`)
         .then(response => response.json())
         .then(json => {
 
+            // if city not found, return error 404
             if(json.cod === '404') {
                 container.style.height = '400px';
                 weatherBox.style.display = 'none';
@@ -25,15 +30,18 @@ search.addEventListener('click', () => {
                 return;
             }
 
+            // if city found, remove error 404
             error404.style.display = 'none';
             error404.classList.remove('fadeIn');
 
+            // get weather data
             const image = document.querySelector('.weather-box img');
             const temperature = document.querySelector('.weather-box .temperature');
             const description = document.querySelector('.weather-box .description');
             const humidity = document.querySelector('.weather-details .humidity span');
             const wind = document.querySelector('.weather-details .wind span');
 
+            // set weather data and apply images
             switch (json.weather[0].main) {
                 case 'Clear':
                     image.src = 'images/clear.png';
@@ -59,11 +67,13 @@ search.addEventListener('click', () => {
                     image.src = '';
             }
 
+            // set weather data, and parse and style where needed
             temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
             description.innerHTML = `${json.weather[0].description}`;
             humidity.innerHTML = `${json.main.humidity}%`;
             wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
 
+            // show weather data to user with animations defined in css
             weatherBox.style.display = '';
             weatherDetails.style.display = '';
             weatherBox.classList.add('fadeIn');
